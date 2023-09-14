4cc81acc19e9af704db8e6c2e3594ae6a56c9fc3
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-09-14 09:57:34 -0400

    add emailjs and temporary route/controller for backend testing

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
-const { Signup, Login, EditTrackedLocations, UpdateSendEmail } = require('../controllers/auth')
+const { Signup, Login, EditTrackedLocations, UpdateSendEmail, SendEmail } = require('../controllers/auth')
 const {userVerification} = require('../middleware/authentication')
 const router = require('express').Router()
 
 router.post('/signup', Signup)
 router.post('/login', Login)
 router.post('/', userVerification)
+router.post('/sendEmail', SendEmail)
 router.put('/editTrackedLocations', EditTrackedLocations)
 router.put('/updateSendEmail', UpdateSendEmail)
 
