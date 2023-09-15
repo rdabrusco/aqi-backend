550138226370feefb16fe73499e3f1bfbc88bc25
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-09-15 13:34:25 -0400

    add universal get  to load page

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ require('./scheduledEmails');
 
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+})
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
