4d69d66942350cd79372a1e399e0fd238f83adab
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-09-16 16:00:48 -0400

    uninstall passport-local-mongoose

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
-// const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const UserSchema = new mongoose.Schema({
