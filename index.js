58edcbf28126a9205839d6e1ea532f78d9fbfc8c
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-09-16 11:23:08 -0400

    remove uncessary universal call, fix express static route

--- index.js
+++ index.js
@@ -34,15 +34,14 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+
+
 app.use("/", authRoute);
 
 require('./scheduledEmails');
 
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
-})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
